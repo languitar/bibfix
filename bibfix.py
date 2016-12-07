3a124372af6db8b5d8959991d66a930c663fa5de
Johannes Wienke <languitar@semipol.de>
2016-12-07 18:40:54 +0100

    Option to execute arbitrary code

--- bibfix.py
+++ bibfix.py
@@ -30,6 +30,15 @@ def main():
         help='Abbreviate common conference and journal title parts.')
 
     parser.add_argument(
+        '-x', '--execute',
+        type=argparse.FileType(),
+        help='Specifies a python file with custom transformation to apply to '
+             'each entry. For this purpose, a single function '
+             'transform(entry) needs to be defined. This is performed after '
+             'all other transformation and therefore allows to clean up '
+             'errors.')
+
+    parser.add_argument(
         'infile',
         metavar='INFILE',
         help='The bibtex file to process')
@@ -39,6 +48,13 @@ def main():
         help='The bibtex file to write to or - for stdout.')
 
     args = parser.parse_args()
+
+    # prepare -x argument
+    if args.execute:
+        exec_globals = {}
+        eval(compile(args.execute.read(), args.execute.name, 'exec'),
+             exec_globals)
+        globals()['transform'] = exec_globals['transform']
 
     parser = bibtexparser.bparser.BibTexParser()
     parser.ignore_nonstandard_types = False
@@ -83,6 +99,10 @@ def main():
                 for source, target in journal_replacements:
                     entry['journal'] = entry['journal'].replace(
                         source, target)
+
+    if args.execute:
+        for entry in database.entries:
+            transform(entry)
 
     if args.outfile == '-':
         bibtexparser.dump(database, sys.stdout)
